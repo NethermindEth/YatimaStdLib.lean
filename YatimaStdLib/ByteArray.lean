70f65748c5ee322cdc69f8d7eb8fceb2ebaa8711
Daniel Britten <Coda-Coda@users.noreply.github.com>
2026-01-21 17:04:55 +1300

    Use `ofList` and `foldlIdx`

--- YatimaStdLib/ByteArray.lean
+++ YatimaStdLib/ByteArray.lean
@@ -5,12 +5,12 @@ namespace ByteArray
 
 /-- Read Nat from Little-Endian ByteArray -/
 def asLEtoNat (b : ByteArray) : Nat :=
-  b.data.data.enum.foldl (init := 0)
-    fun acc (i, bᵢ) => acc + bᵢ.toNat.shiftLeft (i * 8)
+  b.data.toList.foldlIdx (init := 0)
+    fun i acc bᵢ => acc + bᵢ.toNat.shiftLeft (i * 8)
 
 /-- Read Nat from Big-Endian ByteArray -/
 def asBEtoNat (b : ByteArray) : Nat :=
-  b.data.data.foldl (init := 0) fun acc bᵢ => acc.shiftLeft 8 + bᵢ.toNat
+  b.data.toList.foldl (init := 0) fun acc bᵢ => acc.shiftLeft 8 + bᵢ.toNat
 
 /-- Returns the index of -/
 def leadingZeroBits (bytes : ByteArray) : Nat := Id.run do
@@ -34,7 +34,7 @@ def beq : @& ByteArray → @& ByteArray → Bool :=
   beqL
 
 def ordL (a b : ByteArray) : Ordering :=
-  compare a.data.data b.data.data
+  compare a.data.toList b.data.toList
 
 @[extern "lean_byte_array_ord"]
 def ord : @& ByteArray → @& ByteArray → Ordering :=
@@ -49,7 +49,7 @@ instance : DecidableEq ByteArray
     | isFalse h₂ => isFalse $ fun h => by cases h; exact (h₂ rfl)
 
 def Subarray.asBA (s : Subarray UInt8) : ByteArray :=
-  s.array.data.toByteArray
+  s.array.toList.toByteArray
 
 def toString (bs : ByteArray) : String := Id.run do
   if bs.isEmpty then "b[]" else
@@ -102,7 +102,7 @@ def sliceL (bs : ByteArray) (i n : Nat) : ByteArray :=
     | 0, _ => acc
     | n, [] => acc ++ (.replicate n 0)
     | n + 1, b :: bs => aux (acc.push b) n bs
-  .mk $ aux #[] n (bs.data.data.drop i)
+  .mk $ aux #[] n (bs.data.toList.drop i)
 
 @[extern "lean_byte_array_slice"]
 def slice : @& ByteArray → Nat → Nat → ByteArray :=
