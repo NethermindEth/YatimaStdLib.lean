c342bb13495956e2f0bf4c74d1e3d41743e4bdd9
Daniel Britten <Coda-Coda@users.noreply.github.com>
2026-01-21 16:50:35 +1300

    Fix proofs, just use `rfl`

--- YatimaStdLib/UInt.lean
+++ YatimaStdLib/UInt.lean
@@ -40,7 +40,7 @@ def UInt16.toByteArray : UInt16 → ByteArray :=
   UInt16.toByteArrayL
 
 theorem UInt16.toByteArray_size_2 : (UInt16.toByteArray n).size = 2 := by
-  simp [ByteArray.size, Array.size, List.length]
+  rfl
 
 def UInt32.toByteArrayL (n : UInt32) : ByteArray :=
   let a₀ := n.toUInt8
@@ -57,7 +57,7 @@ def UInt32.toByteArray : UInt32 → ByteArray :=
   UInt32.toByteArrayL
 
 theorem UInt32.toByteArray_size_4 : (UInt32.toByteArray n).size = 4 := by
-  simp [ByteArray.size, Array.size, List.length]
+  rfl
 
 def UInt64.toByteArrayL (n : UInt64) : ByteArray :=
   let a₀ := n.toUInt8
@@ -82,4 +82,4 @@ def UInt64.toByteArray : UInt64 → ByteArray :=
   UInt64.toByteArrayL
 
 theorem UInt64.toByteArray_size_8 : (UInt64.toByteArray n).size = 8 := by
-  simp [ByteArray.size, Array.size, List.length]
+  rfl
