512f1377d6b8b04692dd3f6143cf71d0ac893734
Daniel Britten <Coda-Coda@users.noreply.github.com>
2026-01-21 15:38:16 +1300

    `simp_arith` to `simp +arith`

--- YatimaStdLib/Nat.lean
+++ YatimaStdLib/Nat.lean
@@ -108,8 +108,8 @@ theorem div2_lt (h : n ≠ 0) : n / 2 < n := by
   | n+4 =>
     rw [div_eq, if_pos]
     refine succ_lt_succ (Nat.lt_trans ?_ (lt_succ_self _))
-    exact @div2_lt (n + 2) (by simp_arith)
-    simp_arith
+    exact @div2_lt (n + 2) (by simp +arith)
+    simp +arith
 
 /--
 Evaluates `b^e mod m`
